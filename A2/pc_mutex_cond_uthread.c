9ceb1daec6fec8c8889fcc4ffe19603a4cfe6032
Nathan Marcotte <nmarcotte@f-cream.csc.uvic.ca>
2019-02-28 20:40:07 -0800

    challenge 1 and uthread challenge 2 done

--- A2/pc_mutex_cond_uthread.c
+++ A2/pc_mutex_cond_uthread.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include <pthread.h>
+#include "uthread.h"
+#include "uthread_mutex_cond.h"
+#include "spinlock.h"
+
 #define MAX_ITEMS 10
 const int NUM_ITERATIONS = 200;
 const int NUM_CONSUMERS  = 2;
@@ -11,21 +14,21 @@ int producer_wait_count;     // # of times producer had to wait
 int consumer_wait_count;     // # of times consumer had to wait
 int histogram [MAX_ITEMS+1]; // histogram [i] == # of times list stored i items
 
-
-pthread_mutex_t mutex; 
-pthread_cond_t max;
-pthread_cond_t none;
+//spinlock_t lock;
+uthread_mutex_t mutex; 
+uthread_cond_t max;
+uthread_cond_t none;
 
 int items = 0;
 
 void* producer (void* v) {
   for (int i=0; i<NUM_ITERATIONS; i++) {
    
-    pthread_mutex_lock(&mutex);
+    uthread_mutex_lock(mutex);
      
       while(items>=MAX_ITEMS){
         producer_wait_count++;
-        pthread_cond_wait(&none,&mutex);
+        uthread_cond_wait(none);
       }
 
       
@@ -35,8 +38,8 @@ void* producer (void* v) {
     items++;
     histogram[items]++;
     
-    pthread_cond_signal(&max);
-    pthread_mutex_unlock(&mutex);
+    uthread_cond_signal(max);
+    uthread_mutex_unlock(mutex);
   }
   return NULL;
 }
@@ -44,49 +47,51 @@ void* producer (void* v) {
 void* consumer (void* v) {
   for (int i=0; i<NUM_ITERATIONS; i++) {
        
-      pthread_mutex_lock(&mutex);
+      uthread_mutex_lock(mutex);
       
       while(items<=0){
         consumer_wait_count++;
-        pthread_cond_wait(&max,&mutex);
+        uthread_cond_wait(max);
       }
    
-    assert(items>=0);
-    items--;
-    histogram[items]++;
+      assert(items>=0);
+      items--;
+      histogram[items]++;
    
-     pthread_cond_signal(&none);
-     pthread_mutex_unlock(&mutex);
+     uthread_cond_signal(none);
+     uthread_mutex_unlock(mutex);
   }
   return NULL;
 }
 
-
-
 int main (int argc, char** argv) {
-  pthread_t t[4];
-  pthread_mutex_init(&mutex, NULL);
-  pthread_cond_init(&max,NULL);
-  pthread_cond_init(&none,NULL);
+  uthread_t t[4];
+  uthread_init (4);
+  mutex = uthread_mutex_create();
+  max =  uthread_cond_create(mutex);
+  none =  uthread_cond_create(mutex);
+  //spinlock_create(&lock);
+ 
+  
   // TODO: Create Threads and Join
 
 for(int i = 0;i<NUM_PRODUCERS;i++){
-  pthread_create(&t[i],NULL,&producer,NULL);
+  t[i]=uthread_create(producer,NULL);
 }
 
 for(int i = NUM_PRODUCERS;i<NUM_PRODUCERS+NUM_CONSUMERS;i++){
-  pthread_create(&t[i],NULL,&consumer,NULL);
+  t[i]=uthread_create(consumer,NULL);
 }
+
   
 for(int i = 0;i<NUM_PRODUCERS+NUM_CONSUMERS;i++){
-  pthread_join(t[i],NULL);
+  uthread_join(t[i],0);
 }
  
+ uthread_mutex_destroy(mutex);
+ uthread_cond_destroy(max);
+ uthread_cond_destroy(none);
   
-pthread_mutex_destroy(&mutex);
-pthread_cond_destroy(&max);
-pthread_cond_destroy(&none);
-
   printf ("producer_wait_count=%d\nconsumer_wait_count=%d\n", producer_wait_count, consumer_wait_count);
   printf ("items value histogram:\n");
   int sum=0;
@@ -94,6 +99,5 @@ pthread_cond_destroy(&none);
     printf ("  items=%d, %d times\n", i, histogram [i]);
     sum += histogram [i];
   }
-  assert (sum == sizeof (t) / sizeof (pthread_t) * NUM_ITERATIONS);
+  assert (sum == sizeof (t) / sizeof (uthread_t) * NUM_ITERATIONS);
 }
-
--- A2/pc_spinlock_uthread.c
+++ A2/pc_spinlock_uthread.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
-#include <pthread.h>
+#include "uthread.h"
+#include "uthread_mutex_cond.h"
+#include "spinlock.h"
+
 #define MAX_ITEMS 10
 const int NUM_ITERATIONS = 200;
 const int NUM_CONSUMERS  = 2;
@@ -11,82 +14,88 @@ int producer_wait_count;     // # of times producer had to wait
 int consumer_wait_count;     // # of times consumer had to wait
 int histogram [MAX_ITEMS+1]; // histogram [i] == # of times list stored i items
 
-
-pthread_mutex_t mutex; 
-pthread_cond_t max;
-pthread_cond_t none;
-
+spinlock_t lock;
 int items = 0;
 
 void* producer (void* v) {
   for (int i=0; i<NUM_ITERATIONS; i++) {
-   
-    pthread_mutex_lock(&mutex);
-     
+    while(1){
+
       while(items>=MAX_ITEMS){
+        spinlock_lock(&lock);
         producer_wait_count++;
-        pthread_cond_wait(&none,&mutex);
+        spinlock_unlock(&lock);
       }
 
-      
-    assert(items<=MAX_ITEMS);
-      
+      spinlock_lock(&lock);
+
+      if(items >= MAX_ITEMS){
+        spinlock_unlock(&lock);
+      } else {
+          break;
+      }
+
+
+    }
 
     items++;
     histogram[items]++;
-    
-    pthread_cond_signal(&max);
-    pthread_mutex_unlock(&mutex);
+    assert(items<=MAX_ITEMS);
+    spinlock_unlock(&lock);
+
   }
   return NULL;
 }
 
 void* consumer (void* v) {
   for (int i=0; i<NUM_ITERATIONS; i++) {
-       
-      pthread_mutex_lock(&mutex);
-      
+       while(1){
+
       while(items<=0){
+        spinlock_lock(&lock);
         consumer_wait_count++;
-        pthread_cond_wait(&max,&mutex);
+        spinlock_unlock(&lock);
       }
-   
-    assert(items>=0);
+
+      spinlock_lock(&lock);
+
+      if(items <= 0){
+        spinlock_unlock(&lock);
+      } else {
+          break;
+      }
+
+
+    }
+
     items--;
     histogram[items]++;
-   
-     pthread_cond_signal(&none);
-     pthread_mutex_unlock(&mutex);
+    assert(items>=0);
+    spinlock_unlock(&lock);
   }
   return NULL;
 }
 
-
-
 int main (int argc, char** argv) {
-  pthread_t t[4];
-  pthread_mutex_init(&mutex, NULL);
-  pthread_cond_init(&max,NULL);
-  pthread_cond_init(&none,NULL);
+  uthread_t t[4];
+  uthread_init (4);
+  spinlock_create(&lock);
   // TODO: Create Threads and Join
 
 for(int i = 0;i<NUM_PRODUCERS;i++){
-  pthread_create(&t[i],NULL,&producer,NULL);
+  t[i]=uthread_create(producer,NULL);
 }
 
 for(int i = NUM_PRODUCERS;i<NUM_PRODUCERS+NUM_CONSUMERS;i++){
-  pthread_create(&t[i],NULL,&consumer,NULL);
+  t[i]=uthread_create(consumer,NULL);
 }
+
   
 for(int i = 0;i<NUM_PRODUCERS+NUM_CONSUMERS;i++){
-  pthread_join(t[i],NULL);
+  uthread_join(t[i],0);
 }
  
   
-pthread_mutex_destroy(&mutex);
-pthread_cond_destroy(&max);
-pthread_cond_destroy(&none);
-
   printf ("producer_wait_count=%d\nconsumer_wait_count=%d\n", producer_wait_count, consumer_wait_count);
   printf ("items value histogram:\n");
   int sum=0;
@@ -94,6 +103,5 @@ pthread_cond_destroy(&none);
     printf ("  items=%d, %d times\n", i, histogram [i]);
     sum += histogram [i];
   }
-  assert (sum == sizeof (t) / sizeof (pthread_t) * NUM_ITERATIONS);
+  assert (sum == sizeof (t) / sizeof (uthread_t) * NUM_ITERATIONS);
 }
-
